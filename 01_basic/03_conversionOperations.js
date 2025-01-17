4f9a8983e1653fe12d562c1ff040df51d419080e
Shivam Verma <119227879+shivam5983@users.noreply.github.com>
2025-01-17 08:07:40 +0000

    Conversion of Datatypes

---
+++ 01_basic/03_conversionOperations.js
@@ -0,0 +1,16 @@
+/*
+
+// "33" => 33
+// "33abc" => NaN(Not a number)
+// true => 1;  false => 0;
+*/
+
+let loggedIn= 1
+
+let booleanIsLoggedIn = Boolean(loggedIn)
+console.log(typeof(booleanIsLoggedIn));
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> False
+//  " " => false
+// "Shivam" => true 
