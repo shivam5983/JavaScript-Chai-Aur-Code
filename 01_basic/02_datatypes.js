b950c8a825a1a716e1bfd743f06e8ff9a5649075
Shivam Verma <119227879+shivam5983@users.noreply.github.com>
2025-01-17 07:38:03 +0000

    dataTypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,22 @@
+"Use strict";  // treat all JS code as newer version
+
+// alert(3+3) we are using nodejs ,not browser
+
+let name="Shiv";
+let age=23;
+let isLoggedIn=false
+
+//    DataTypes
+
+//number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined); //undefined 
+console.log(typeof null); //object
