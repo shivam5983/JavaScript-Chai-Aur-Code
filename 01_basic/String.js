16a32b870327e2c0fa964b48db41b4eb78593e01
Shivam Verma <119227879+shivam5983@users.noreply.github.com>
2025-01-20 07:07:37 +0000

    String

---
+++ 01_basic/String.js
@@ -0,0 +1,44 @@
+const name="Shivam"
+const repocount= 44;
+
+console.log(name+repocount+" Value");   
+//output Shivam44 Value
+
+
+console.log(`Hello my name is ${name} and my repo count is ${repocount}`)
+
+// to declare string
+const gameName= new String("Shiv-am-smile")
+//  Or const gameName="Shivam"
+
+console.log(gameName[1])
+console.log(gameName.__proto__);
+
+
+
+
+
+console.log(gameName.length);  //output: 8 
+console.log(gameName.toUpperCase()); 
+// yeh Original String ko change nhi kiya kyoki yeh stack memory mei ja rahe hai 
+
+console.log(gameName.charAt(4)); 
+console.log(gameName.indexOf('a')); 
+
+const newString = gameName.substring(0,4) 
+console.log(newString) // output shiv
+
+const anotherString= gameName.slice(-5,11)
+console.log(anotherString);
+
+
+const newStringOne= "   Hitesh    "
+console.log(newStringOne.trim())
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20','-'))
+
+// url.includes('shivam')  output false
+
+console.log(gameName.split('-'))
