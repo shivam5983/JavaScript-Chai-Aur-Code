f2c650b96c307ecbdad9e12bf8772fe01f7bb2d8
Shivam Verma <119227879+shivam5983@users.noreply.github.com>
2025-01-20 06:00:35 +0000

    datatypeSummary

--- 01_basic/03_conversionOperations.js
+++ 01_basic/03_conversionOperations.js
@@ -5,12 +5,38 @@
 // true => 1;  false => 0;
 */
 
-let loggedIn= 1
+// let loggedIn= 1
 
-let booleanIsLoggedIn = Boolean(loggedIn)
-console.log(typeof(booleanIsLoggedIn));
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(loggedIn)
+// console.log(typeof(booleanIsLoggedIn));
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0=> False
 //  " " => false
 // "Shivam" => true 
+
+
+// ******************** Operations ****************************
+
+let value=3;
+let negValue= -value;
+
+// console.log(negValue);
+
+let str1= "hello"
+let str2= " Shivam"
+let str3= str1 +str2
+// console.log(str3);
+
+
+console.log("1" + 2);    //12
+console.log(1 + "2");      //12 
+console.log("1" + 2 + 2);    //122
+console.log(1 + 2+ "2");   //  32
+
+// console.log(+true);  // ouput=1
+
+let gamecounter=100;
+++gamecounter
+console.log(gamecounter);
+
---
+++ 01_basic/datarypes-summary.js
@@ -0,0 +1,42 @@
+// primitive data Types  Call by value 
+
+// 7 types: String , Number, Boolean , Null , undefined, Symbol, BigInt
+
+
+// javascript is a dynamically type
+
+// Reference type (Non Primitive)
+
+// array , Objects , Functions
+
+// Array ka typeof "Function" aayega
+// objects ka typeof "Function" aayega
+// FUnctions ka typeof "Object function" aayega
+// Null ka typeof "Object" aayega
+
+
+const id= Symbol('123')
+const anotherId =Symbol('123')
+
+// console.log(id===anotherId);   //false
+
+
+const bigNumber= 324564645565465465464n
+
+const heros = ["Shaktiman","Doga","nagraj"]
+
+let myObj={
+    name: "Hitesh",
+    age:22,
+}
+
+console.log(typeof(myobj));
+
+
+// const myFunc= function(){
+//     console.log("Heelo World")
+
+// }
+
+
+
